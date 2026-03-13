dd5eca05d3c0f6620112fd9ec50a428920e36efd
sparkmig <301mikkel103@gmail.com>
2026-03-14 00:19:59 +0100

    Added health check to order microservice

---
+++ src/DemoShop.Backend/OrderService/HealthChecks/AvailabiltyHealthCheck.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.Diagnostics.HealthChecks;
+
+namespace OrderService.Api.HealthChecks
+{
+    public class AvailabiltyHealthCheck : IHealthCheck
+    {
+        public static string Name => nameof(AvailabiltyHealthCheck);
+        public Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context, CancellationToken cancellationToken = default)
+        {
+            return Task.FromResult(HealthCheckResult.Healthy("The service is available."));
+        }
+    }
+}
--- src/DemoShop.Backend/OrderService/Program.cs
+++ src/DemoShop.Backend/OrderService/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+using OrderService.Api.HealthChecks;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,8 +8,29 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
-
+builder.Services.AddHealthChecks().AddCheck<AvailabiltyHealthCheck>(AvailabiltyHealthCheck.Name);
 var app = builder.Build();
+
+app.UseHealthChecks("/health", new HealthCheckOptions()
+{
+    ResponseWriter = async (context, report) =>
+    {
+        context.Response.ContentType = "application/json";
+        var result = System.Text.Json.JsonSerializer.Serialize(
+            new
+            {
+                status = report.Status.ToString(),
+                checks = report.Entries.Select(e => new
+                {
+                    name = e.Key,
+                    status = e.Value.Status.ToString(),
+                    exception = e.Value.Exception?.Message,
+                    duration = e.Value.Duration.ToString()
+                })
+            });
+        await context.Response.WriteAsync(result);
+    }
+});
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
